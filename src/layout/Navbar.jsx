211fa35b07b50ee65efb678f768ca8fb421810a7
Bhagyashri723 <bhagyashrikhond11@gmail.com>
2026-05-07 12:13:32 +0530

    navbar fix mobile

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
         <div className="flex items-center justify-center gap-4 mt-2 ">
 
-        <nav className="flex justify-center gap-4 text-sm mt-2 md:mt-0 md:absolute md:left-1/2 md:-translate-x-1/2 md:gap-8 md:text-base">
+        <nav className="flex justify-center gap-2 md:gap-4 text-sm mt-2 md:mt-0 md:absolute md:left-1/2 md:-translate-x-1/2 md:text-base">
 
             <a href="#home" className='  text-white'>Home</a>
             <a href="#about" className='  text-white'>About</a>
